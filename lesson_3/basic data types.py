e19ac755460174188dd2f29c27ff6d8d49a89c21
kris9955 <auscaroh9@gmail.com>
2023-01-17 04:52:23 +0300

    Создание файла с базовыми типами данных

---
+++ lesson_3/basic data types.py
@@ -0,0 +1,46 @@
+int,float
+a = 5
+b = 6
+c = 1.5
+d = 2
+print(a+b, a-b, a * b, a / b, a ** b)
+print(b % d, a % d)
+print(b // d, b // a)
+print(type(a))
+print(type(c))
+print(round(c))
+print(c+d, c*d)
+
+
+
+str
+text = 'Hello world'.replace('w','G').replace('r','')
+print(text)
+text1 = 'thErE'
+print(text + ' ' + text1)
+print(text1.title(), text1.upper(), text1.capitalize(), text1.swapcase())
+print(text1.count('E'))
+
+text2 = 'Hi '
+text3 = 'Chris'
+print(''.join([text2, text3]))
+
+str_ = 'hi'
+str_2 = 'hi1'
+print(id(str_))
+print(id(str_2))
+
+bool
+number = 5
+number1 = 5
+print(number < number1, number > number1)
+print(bool(0), bool(1), bool(-1), bool(2))
+print(type(False))
+print(bool(' '), bool(''))
+
+
+
+
+
+
+
